a4f63ea76aa9603795089a883947eb3644147cca
Sui Tools <suitools01@gmail.com>
2025-06-15 17:36:52 +0530

    fix: Use non-deprecated TemplateContextType constructor
    - Removed deprecated two-parameter constructor
    - Now using single-parameter constructor with presentableName only
    - contextId is already properly set in plugin.xml
    - Fixes deprecation warning in JetBrains plugin verification

--- src/main/kotlin/com/suimove/intellij/templates/MoveTemplateContextType.kt
+++ src/main/kotlin/com/suimove/intellij/templates/MoveTemplateContextType.kt
@@ -4,7 +4,7 @@ import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 import com.suimove.intellij.psi.MoveFile
 
-class MoveTemplateContextType : TemplateContextType("MOVE", "Move") {
+class MoveTemplateContextType : TemplateContextType("Move") {
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
         return templateActionContext.file is MoveFile
     }
